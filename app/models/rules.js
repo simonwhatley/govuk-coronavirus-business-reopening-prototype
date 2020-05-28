293f1d6dcd8ce081a135a231082c8079768b84ad
Simon Whatley <simonwhatley@gmail.com>
2020-05-28 14:29:07 +0100

    Update questions and rules

--- app/models/rules.js
+++ app/models/rules.js
@@ -9,39 +9,182 @@ exports.find = function(answers) {
   // --------------------------------------------------
   // TEMPLATE
   // --------------------------------------------------
+  //
+  // if (answers['QUESTION'] !== undefined) {
+  //
+  //   if (answers['QUESTION'] == 'ANSWER') {
+  //     outcomes.push('SCHEME');
+  //   }
+  //
+  // }
 
-  if (answers['QUESTION'] !== undefined) {
+  outcomes.push('risk_assessment');
 
-    if (answers['QUESTION'] == 'ANSWER') {
-      outcomes.push('SCHEME');
+  outcomes.push('on_site_or_remote');
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('restaurants') !== -1) {
+      outcomes.push('restaurants_takeaway_delivery');
     }
 
   }
 
-  outcomes.push('risk_assessment');
-  outcomes.push('on_site_or_remote');
-  outcomes.push('restaurants_takeaway_delivery');
   outcomes.push('social_distancing');
-  outcomes.push('construction_social_distancing');
-  outcomes.push('labs_social_distancing');
-  outcomes.push('offices_social_distancing');
-  outcomes.push('home_social_distancing');
-  outcomes.push('restaurants_social_distancing');
-  outcomes.push('shops_social_distancing');
-  outcomes.push('vehicle_social_distancing');
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('construction') !== -1) {
+      outcomes.push('construction_social_distancing');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('research') !== -1) {
+      outcomes.push('labs_social_distancing');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('offices') !== -1) {
+      outcomes.push('offices_social_distancing');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('homes') !== -1) {
+      outcomes.push('home_social_distancing');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('restaurants') !== -1) {
+      outcomes.push('restaurants_social_distancing');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('shops') !== -1) {
+      outcomes.push('shops_social_distancing');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('vehicles') !== -1) {
+      outcomes.push('vehicle_social_distancing');
+    }
+
+  }
+
   outcomes.push('cleaning');
-  outcomes.push('construction_cleaning');
-  outcomes.push('factories_cleaning');
-  outcomes.push('labs_cleaning');
-  outcomes.push('offices_cleaning');
-  outcomes.push('home_cleaning');
-  outcomes.push('restaurants_cleaning');
-  outcomes.push('shops_cleaning');
-  outcomes.push('vehicle_cleaning');
-  outcomes.push('visitors');
-  outcomes.push('meetings');
-  outcomes.push('travel');
-  outcomes.push('goods');
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('construction') !== -1) {
+      outcomes.push('construction_cleaning');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('factories') !== -1) {
+      outcomes.push('factories_cleaning');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('research') !== -1) {
+      outcomes.push('labs_cleaning');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('offices') !== -1) {
+      outcomes.push('offices_cleaning');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('homes') !== -1) {
+      outcomes.push('home_cleaning');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('restaurants') !== -1) {
+      outcomes.push('restaurants_cleaning');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('shops') !== -1) {
+      outcomes.push('shops_cleaning');
+    }
+
+  }
+
+  if (answers['sectors'] !== undefined) {
+
+    if (answers['sectors'].indexOf('vehicles') !== -1) {
+      outcomes.push('vehicle_cleaning');
+    }
+
+  }
+
+  if (answers['visitors'] !== undefined) {
+
+    if (answers['visitors'] == 'yes') {
+      outcomes.push('visitors');
+    }
+
+  }
+
+  if (answers['meetings'] !== undefined) {
+
+    if (answers['meetings'] == 'yes') {
+      outcomes.push('meetings');
+    }
+
+  }
+
+  if (answers['travel'] !== undefined) {
+
+    if (answers['travel'] == 'yes') {
+      outcomes.push('travel');
+    }
+
+  }
+
+  if (answers['goods'] !== undefined) {
+
+    if (answers['goods'] == 'yes') {
+      outcomes.push('goods');
+    }
+
+  }
 
   return outcomes;
 }
